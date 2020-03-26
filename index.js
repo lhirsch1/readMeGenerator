e04df4a125e3a60d973f6f23a8f1efef9499ccce
Lukas Hirsch <hirschlukasa@gmail.com>
2020-03-26 15:43:40 -0500

    prompts name and links to github

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const axios = require('axios');
 const fs = require('fs');
 const util = require('util');
+const inquirer = require('inquirer');
 
 const questions = [
 
@@ -11,18 +12,30 @@ function writeToFile(fileName, data) {
 
 function init() {
 
-    axios
-  .get("https://api.github.com/users/lhirsch1")
-  .then(function(res) {
-    console.log(res.data);
-  });
+  //repos https://api.github.com/users/lhirsch1/repos
 
+  inquirer
+    .prompt({
+      message: "Enter your GitHub username:",
+      name: "username"
+    })
+    .then(function ({username}) {
+      console.log(username);
+
+      axios
+        .get(`https://api.github.com/users/${username}`)
+        .then(function (res) {
+          console.log(res.data);
+          //avatar_url
+        });
+
+    });
 }
 
 init();
 
 //take user input for user name
-  //then 
+//prompt user to select repo
 //get git hub info
   // project title, description, table of cont, installation, usage, license, contributing, tests, questions,profile pic, githubemail
 
